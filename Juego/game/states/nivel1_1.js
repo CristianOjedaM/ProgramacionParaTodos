fae2b7a4c4825b7db9495c566a811df3e37c5906
CristianOjedaM <cristianojeda2911@gmail.com>
2015-05-13 17:02:06 -0500

    add onPause

--- Juego/game/states/nivel1_1.js
+++ Juego/game/states/nivel1_1.js
@@ -289,30 +289,35 @@
             if(this.error_score.errorCadena != 0 && (this.error_score.errorCadena%3) == 0 ){
               this.game.add.sprite(this.game.world.centerX - 138, this.game.world.centerY - 90,'MensajeAyuda',frame);
               this.game.paused = true;
+              this.game.onPause.add(onPause,this);
             }                
           break;
           case 1:
             if(this.error_score.errorNumero != 0 && (this.error_score.errorNumero%3) == 0 ){
               this.game.add.sprite(this.game.world.centerX - 138, this.game.world.centerY - 90,'MensajeAyuda',frame);
               this.game.paused = true;
+              this.game.onPause.add(onPause,this);
             }                                  
           break;
           case 2:
             if(this.error_score.errorBool != 0 && (this.error_score.errorBool%3) == 0 ){
               this.game.add.sprite(this.game.world.centerX - 138, this.game.world.centerY - 90,'MensajeAyuda',frame);
               this.game.paused = true;
+              this.game.onPause.add(onPause,this);
             }                                
           break;
           case 3:
             if(this.error_score.errorArray != 0 && (this.error_score.errorArray%3) == 0 ){
               this.game.add.sprite(this.game.world.centerX - 138, this.game.world.centerY - 90,'MensajeAyuda',frame);
               this.game.paused = true;
+              this.game.onPause.add(onPause,this);
             }                                  
           break; 
           case 4:
             if(this.error_score.errorGeneral != 0 && (this.error_score.errorGeneral%5) == 0 ){
               this.game.add.sprite(this.game.world.centerX - 138, this.game.world.centerY - 90,'MensajeAyuda',frame);
               this.game.paused = true;
+              this.game.onPause.add(onPause,this);
             }
           break;             
         }
