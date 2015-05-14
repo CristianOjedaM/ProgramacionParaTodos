3030e508e88e346cd9b7e2e1413091eb92dbdf52
CristianOjedaM <cristianojeda2911@gmail.com>
2015-05-13 22:07:43 -0500

    Se destruye imagen de ayuda

--- Juego/game/states/nivel1_1.js
+++ Juego/game/states/nivel1_1.js
@@ -289,40 +289,40 @@
         switch(tipo){
           case 0:
             if(this.error_score.errorCadena != 0 && (this.error_score.errorCadena%3) == 0 ){
-              this.game.add.sprite(this.game.world.centerX - 138, this.game.world.centerY - 90,'MensajeAyuda',frame);
+              this.MensajeAyuda = this.game.add.sprite(this.game.world.centerX - 138, this.game.world.centerY - 90,'MensajeAyuda',frame);
               this.game.paused = true;
             }                
           break;
           case 1:
             if(this.error_score.errorNumero != 0 && (this.error_score.errorNumero%3) == 0 ){
-              this.game.add.sprite(this.game.world.centerX - 138, this.game.world.centerY - 90,'MensajeAyuda',frame);
+              this.MensajeAyuda =this.game.add.sprite(this.game.world.centerX - 138, this.game.world.centerY - 90,'MensajeAyuda',frame);
               this.game.paused = true;
             }                                  
           break;
           case 2:
             if(this.error_score.errorBool != 0 && (this.error_score.errorBool%3) == 0 ){
-              this.game.add.sprite(this.game.world.centerX - 138, this.game.world.centerY - 90,'MensajeAyuda',frame);
+              this.MensajeAyuda = this.game.add.sprite(this.game.world.centerX - 138, this.game.world.centerY - 90,'MensajeAyuda',frame);
               this.game.paused = true;
             }                                
           break;
           case 3:
             if(this.error_score.errorArray != 0 && (this.error_score.errorArray%3) == 0 ){
-              this.game.add.sprite(this.game.world.centerX - 138, this.game.world.centerY - 90,'MensajeAyuda',frame);
+              this.MensajeAyuda = this.game.add.sprite(this.game.world.centerX - 138, this.game.world.centerY - 90,'MensajeAyuda',frame);
               this.game.paused = true;
             }                                  
           break; 
           case 4:
             if(this.error_score.errorGeneral != 0 && (this.error_score.errorGeneral%5) == 0 ){
-              this.game.add.sprite(this.game.world.centerX - 138, this.game.world.centerY - 90,'MensajeAyuda',frame);
+              this.MensajeAyuda = this.game.add.sprite(this.game.world.centerX - 138, this.game.world.centerY - 90,'MensajeAyuda',frame);
               this.game.paused = true;              
             }
           break;             
         }
     },
     enPausa:function(game,event)
-    {
-      console.log('Entro');
+    {      
       if(game.game.paused){
+        this.MensajeAyuda.destroy();
         game.game.paused = false;    
       }      
     }
