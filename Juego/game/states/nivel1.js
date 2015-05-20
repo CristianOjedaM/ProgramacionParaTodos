c2ae8b5fc0a82bd00b39133ae4342e115156ef70
CristianOjedaM <cristianojeda2911@gmail.com>
2015-05-20 14:02:10 -0500

    Reanudar en cada nivel

--- Juego/game/states/nivel1.js
+++ Juego/game/states/nivel1.js
@@ -108,7 +108,7 @@
       //Se incluye el panel de pausa al nivel
       this.pnlPausa = new Pausa(this.game);
       this.game.add.existing(this.pnlPausa);
-      this.game.input.onDown.add(this.pausaJuego(),this);
+      this.game.input.onDown.add(this.pausaJuego,this);
     },
 
     update: function() {
