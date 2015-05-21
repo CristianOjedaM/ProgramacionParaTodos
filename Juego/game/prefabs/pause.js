85d80855c9364814f59ca3f67e7080a2c32415d8
CristianOjedaM <cristianojeda2911@gmail.com>
2015-05-21 12:53:08 -0500

    na

--- Juego/game/prefabs/pause.js
+++ Juego/game/prefabs/pause.js
@@ -51,7 +51,7 @@
   Pause.prototype.reset = function(game){
      //this.game.state.getCurrentState().game.state.restart(true);
      this.game.state.clearCurrentState();
-     game.state.getCurrentState().game.state.start(this.game.state.current);
+     game.state.start(game.state.current);
   };
  
   module.exports = Pause;
--- Juego/game/states/nivel1.js
+++ Juego/game/states/nivel1.js
@@ -274,7 +274,7 @@
         }
       }else if(game.x > (this.game.width - 130) && game.x < (this.game.width - 85) && game.y > y1 && game.y < y2 ){
          //Se esconde el panel de pausa        
-          this.pnlPausa.reset(game);
+          this.pnlPausa.reset(this.game);
       }
     }
   };
