83135ff8dfea865e69eabfe24d6da4e8664681ca
CristianOjedaM <cristianojeda2911@gmail.com>
2015-06-03 20:07:44 -0500

    na

--- Juego/game/states/nivel3.js
+++ Juego/game/states/nivel3.js
@@ -49,8 +49,8 @@
     },
 
     iniciarJuego : function(game){
-      var x1 = 531;
-      var x2 = 680;
+      var x1 = 115;
+      var x2 = 264;
       var y1 = 480;
       var y2 = 550;
       if(game.x > x1 && game.x < x2 && game.y > y1 && game.y < y2 ){
