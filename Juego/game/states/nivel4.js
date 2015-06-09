f929a2ff458eb5a5a50bd7b5a50b256080f0027e
CristianOjedaM <cristianojeda2911@gmail.com>
2015-06-09 12:21:36 -0500

    nivel 4

--- Juego/game/states/nivel4.js
+++ Juego/game/states/nivel4.js
@@ -3,15 +3,24 @@
 
 
   function Nivel4() {}
-  Nivel5.prototype = {
-
+  Nivel4.prototype = {
+    vel:50,//Velocidad de inicio para movimiento de items
   	create: function() {
-	  this.game.world.setBounds(0, 0, 800, 600);
+      //Habilitacion de fisicas
+      this.game.physics.startSystem(Phaser.Physics.ARCADE);
+	    this.game.world.setBounds(0, 0, 800, 600);
       //Fondo de juego
       this.game.add.tileSprite(0, 0,800,600, 'Fondo4');
   	},
 
+  	update: function(){
 
+  	},
+
+    crearCarro: function(){
+      var carro = this.items.create(-100,550,'Carro');
+      carro.body.velocity.x = this.vel;
+    }
   };
 
   module.exports = Nivel4;
