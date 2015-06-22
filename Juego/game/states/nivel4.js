e92641a46bd5fcfec560c7445c9c0809b07f0cf5
CristianOjedaM <cristianojeda2911@gmail.com>
2015-06-22 08:46:31 -0500

    Nivel 4.1.9

--- Juego/game/states/nivel4.js
+++ Juego/game/states/nivel4.js
@@ -16,6 +16,9 @@
     intSituacion:0,
     itemX: 0,
     itemY: 0,
+    slotCondicion:false,
+    slotAccion_1:false,
+    slotAccion_2:false,
   	create: function() {
       //Habilitacion de fisicas
       this.game.physics.startSystem(Phaser.Physics.ARCADE);      
@@ -56,8 +59,9 @@
 
       Situacion[this.intSituacion].acciones.forEach(function(acciontext) {
           var item = CItems.create(495,yitem,'accion_small');
+          item.tipo = 0;
           item.anchor.setTo(0.5,0.5);
-          item.texto = game.game.add.text(item.x, item.y,acciontext , { font: '12px calibri', fill: '#000', align:'center'});
+          item.texto = game.game.add.text(item.x, item.y,acciontext , { font: '14px calibri', fill: '#fff', align:'center'});
           item.texto.anchor.setTo(0.5,0.5);
           item.inputEnabled = true;
           item.events.onInputDown.add(game.clickItem, game);
@@ -68,9 +72,10 @@
       //creamos las condiciones de la situación
       yitem = 350;
       Situacion[this.intSituacion].condiciones.forEach(function(condiciontext) {
-          var item = CItems.create(630,yitem,'condicion');
+          var item = CItems.create(630,yitem,'condicion');          
+          item.tipo = 1;
           item.anchor.setTo(0.5,0.5);
-          item.texto = game.game.add.text(item.x, item.y,condiciontext , { font: '12px calibri', fill: '#000', align:'center'});
+          item.texto = game.game.add.text(item.x, item.y,condiciontext , { font: '10px calibri', fill: '#fff', align:'center'});
           item.texto.anchor.setTo(0.5,0.5);
           item.inputEnabled = true;
           item.events.onInputDown.add(game.clickItem, game);
@@ -89,19 +94,43 @@
     releaseItem:function(item){
       if(item.movimiento){
         item.movimiento = false;
-        if(item.body.y >= (this.slot.body.y + 40) && item.body.y <= (this.slot.body.y + 104) && item.body.x >= (this.slot.body.x + 38) && item.body.x <= (this.slot.body.x + 270) ){
-          var itemEncajado = this.items.create( (this.slot.body.x + 153),(this.slot.body.y + 71),'accion_large');
-          itemEncajado.anchor.setTo(0.5,0.5);
-          itemEncajado.texto = item.texto;
-          itemEncajado.texto.fontSize = 20;
-          itemEncajado.texto.x = itemEncajado.x;
-          itemEncajado.texto.y = itemEncajado.y;          
-          item.kill();
+        //Se define cuadro imaginario para las acciones
+        if(item.tipo == 0 && item.body.y >= (this.slot.body.y + 40) && item.body.y <= (this.slot.body.y + 104) && item.body.x >= (this.slot.body.x + 38) && item.body.x <= (this.slot.body.x + 270) ){
+          if(!this.slotAccion_1){
+            //Creamos el item el cual encaja en el slot de la accion          
+            var itemEncajado = this.items.create( (this.slot.body.x + 154),(this.slot.body.y + 72),'accion_large');
+            itemEncajado.anchor.setTo(0.5,0.5);
+            itemEncajado.texto = item.texto;
+            itemEncajado.texto.fontSize = 20;
+            itemEncajado.texto.x = itemEncajado.x;
+            itemEncajado.texto.y = itemEncajado.y;
+            itemEncajado.slot1 = true;          
+            item.kill();
+          }else{
+
+            this.items.forEach(function(itemslot1) {
+              if(itemslot1.slot1){
+                var textoAnt = itemslot1.texto;
+                itemslot1.texto = item.texto;
+                itemslot1.texto.fontSize = 20;
+                //actualizamos el item arrastrado con el texto del item en el slot
+                item.texto = textoAnt;
+                itemslot1.texto.fontSize = 14;
+              }
+            }
+            item.x = this.itemX
+            item.y = this.itemY;
+            item.texto.x = item.x;
+            item.texto.y = item.y;
+
+          }
+          //indicamos que el primer slot se ha ocupado
+          this.slotAccion_1 = true;
         }else{
           item.x = this.itemX
           item.y = this.itemY;
-          item.texto.x = item.x ;
-          item.texto.y = item.y ;
+          item.texto.x = item.x;
+          item.texto.y = item.y;
         }
       }
     },
